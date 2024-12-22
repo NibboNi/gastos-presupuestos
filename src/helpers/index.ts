c23c00b427323f3a023c0dc403bb935f4229a5e4
nibbo <131214845+NibboNi@users.noreply.github.com>
2024-12-22 02:05:56 -0600

    add format date function

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -4,3 +4,15 @@ export function formatCurrency(quantity: number) {
     currency: "MXN",
   }).format(quantity);
 }
+
+export function formatDate(dateStr: string): string {
+  const dateObj = new Date(dateStr);
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "2-digit",
+    month: "2-digit",
+    day: "2-digit",
+  };
+
+  return new Intl.DateTimeFormat("es-ES", options).format(dateObj);
+}
