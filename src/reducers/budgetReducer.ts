5ba42138ba91c294f781d431293313da88f9958b
nibbo <131214845+NibboNi@users.noreply.github.com>
2024-12-22 01:08:55 -0600

    add add-expense action

--- src/reducers/budgetReducer.ts
+++ src/reducers/budgetReducer.ts
@@ -1,14 +1,27 @@
+import { v4 as uuidv4 } from "uuid";
+import type { DraftExpense, Expense } from "../types";
+
 export type BudgetActions =
   | { type: "add-budget"; payload: { budget: number } }
   | { type: "show-modal" }
-  | { type: "hide-modal" };
+  | { type: "hide-modal" }
+  | { type: "add-expense"; payload: { expense: DraftExpense } };
 
 export type BudgetState = {
   budget: number;
   modal: boolean;
+  expenses: Expense[];
 };
 
-export const initialState: BudgetState = { budget: 0, modal: false };
+export const initialState: BudgetState = {
+  budget: 0,
+  modal: false,
+  expenses: [],
+};
+
+const createExpense = (draftExpense: DraftExpense): Expense => {
+  return { ...draftExpense, id: uuidv4() };
+};
 
 export const budgetReducer = (
   state: BudgetState = initialState,
@@ -24,6 +37,11 @@ export const budgetReducer = (
 
   if (action.type === "hide-modal") {
     return { ...state, modal: false };
+  }
+
+  if (action.type === "add-expense") {
+    const expense = createExpense(action.payload.expense);
+    return { ...state, expenses: [...state.expenses, expense] };
   }
 
   return { ...state };
