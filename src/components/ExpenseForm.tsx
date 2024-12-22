75a3fdd228844270fdbe12aae4d19f7a9b8f3eae
nibbo <131214845+NibboNi@users.noreply.github.com>
2024-12-22 01:09:20 -0600

    add submit logic

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,18 +1,32 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
+import { useBudget } from "../hooks/useBudget";
 import Select from "./Select";
 import CustomInput from "./CustomInput";
 import type { DraftExpense } from "../types";
 
+const initialExpense: DraftExpense = {
+  name: "",
+  amount: 0,
+  category: "",
+  date: "",
+};
+
 export default function ExpenseForm() {
-  const [expense, setExpense] = useState<DraftExpense>({
-    name: "",
-    amount: 0,
-    category: "",
-    date: new Date().toDateString(),
-  });
+  const [expense, setExpense] = useState<DraftExpense>(initialExpense);
+  const { dispatch } = useBudget();
+  const isValid = useMemo(() => {
+    return Object.values(expense).includes("") || expense.amount <= 0;
+  }, [expense]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    dispatch({ type: "add-expense", payload: { expense } });
+    setExpense(initialExpense);
+  };
 
   return (
-    <form className="flex flex-col">
+    <form onSubmit={handleSubmit} className="flex flex-col">
       <legend className="py-1 text-2xl text-center capitalize font-extralight border-b border-b-zinc-200 transition-colors duration-300 ease-in-out dark:border-b-zinc-800">
         Nuevo gasto
       </legend>
@@ -47,6 +61,7 @@ export default function ExpenseForm() {
       <input
         type="submit"
         value="Registrar gasto"
+        disabled={isValid}
         className="mt-5 mx-auto py-2 px-5 bg-zinc-950 text-white text-sm font-medium capitalize rounded cursor-pointer transition-colors duration-300 ease-in-out hover:bg-black disabled:opacity-50 disabled:cursor-not-allowed dark:bg-zinc-100 dark:text-black dark:hover:bg-white"
       />
     </form>
