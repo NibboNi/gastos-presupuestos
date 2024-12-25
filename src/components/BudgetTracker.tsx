ffc220919c77f12aef18144267cabb719faa8bd4
nibbo <131214845+NibboNi@users.noreply.github.com>
2024-12-25 00:37:52 -0600

    add reset app action

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -3,18 +3,26 @@ import { formatCurrency } from "../helpers";
 import AmoutDisplay from "./AmoutDisplay";
 
 export default function BudgetTracker() {
-  const { state, remainingBudget, totalExpenses } = useBudget();
+  const { state, remainingBudget, totalExpenses, dispatch } = useBudget();
+  const deg = ((remainingBudget * 360) / state.budget).toFixed(2);
+  const percent = ((remainingBudget * 100) / state.budget).toFixed(2);
 
   return (
     <div className="mx-auto p-5 max-w-7xl grid grid-cols-1 gap-5 md:grid-cols-2">
       <div className="py-5 px-8 flex flex-col items-start bg-black text-white border border-transparent rounded-lg shadow-2xl transition-colors duration-300 ease-in-out dark:bg-zinc-950 dark:border-zinc-800 md:w-2/3 md:justify-self-end">
         <h2 className="text-xl font-medium">Presupuesto</h2>
         <p className="text-lg font-light">{formatCurrency(state.budget)}</p>
-        <div className="mt-2.5 h-1.5 w-full relative bg-zinc-200 rounded-lg overflow-hidden transition-colors">
-          <span
-            className="absolute top-0 left-0 h-full bg-blue-700 transition-all duration-300 ease-in-out"
-            style={{ width: `${(remainingBudget * 100) / state.budget}%` }}
-          ></span>
+        <div
+          className="progress-bar mt-2.5 mx-auto h-32 w-32 relative rounded-full transition-all duration-300 ease-in-out"
+          style={{
+            background: `conic-gradient(#1d4ed8 ${deg}deg, ${
+              +deg === 0 ? "#be185d" : "transparent"
+            } ${deg}deg)`,
+          }}
+        >
+          <p className="absolute top-1/2 left-1/2 font-bold -translate-y-1/2 -translate-x-1/2">
+            {percent}%
+          </p>
         </div>
       </div>
       <div className="p-5 flex flex-col items-start border rounded-lg shadow-md transition-colors duration-300 ease-in-out dark:bg-zinc-900 dark:border-zinc-800 md:w-2/3">
@@ -22,7 +30,10 @@ export default function BudgetTracker() {
         <AmoutDisplay label="Disponible" amount={remainingBudget} />
         <AmoutDisplay label="Gastado" amount={totalExpenses} />
       </div>
-      <button className="mx-auto py-2 px-5 bg-zinc-950 text-white text-sm font-medium capitalize rounded transition-colors duration-300 ease-in-out hover:bg-black disabled:opacity-50 disabled:cursor-not-allowed dark:bg-zinc-100 dark:text-black md:col-start-1 md:col-end-3 dark:hover:bg-white">
+      <button
+        onClick={() => dispatch({ type: "reset-app" })}
+        className="mx-auto py-2 px-5 bg-zinc-950 text-white text-sm font-medium capitalize rounded transition-colors duration-300 ease-in-out hover:bg-black disabled:opacity-50 disabled:cursor-not-allowed dark:bg-zinc-100 dark:text-black md:col-start-1 md:col-end-3 dark:hover:bg-white"
+      >
         Resetear App
       </button>
     </div>
--- src/reducers/budgetReducer.ts
+++ src/reducers/budgetReducer.ts
@@ -8,7 +8,8 @@ export type BudgetActions =
   | { type: "add-expense"; payload: { expense: DraftExpense } }
   | { type: "remove-expense"; payload: { id: Expense["id"] } }
   | { type: "get-expense-by-id"; payload: { id: Expense["id"] } }
-  | { type: "update-expense"; payload: { expense: Expense } };
+  | { type: "update-expense"; payload: { expense: Expense } }
+  | { type: "reset-app" };
 
 export type BudgetState = {
   budget: number;
@@ -85,6 +86,16 @@ export const budgetReducer = (
           : expense
       ),
       modal: false,
+      editingId: "",
+    };
+  }
+
+  if (action.type === "reset-app") {
+    return {
+      ...state,
+      budget: 0,
+      modal: false,
+      expenses: [],
       editingId: "",
     };
   }
