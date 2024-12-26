cd2252a5cca7cf97c306f905c38bca46b744c5ef
nibbo <131214845+NibboNi@users.noreply.github.com>
2024-12-25 19:26:49 -0600

    add color property

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -10,11 +10,16 @@ import {
 import type { Category } from "../types";
 
 export const categories: Category[] = [
-  { id: "1", name: "Ahorro", icon: mdiFinance },
-  { id: "2", name: "Comida", icon: mdiSilverwareVariant },
-  { id: "3", name: "Casa", icon: mdiHome },
-  { id: "4", name: "Gastos Varios", icon: mdiWalletBifold },
-  { id: "5", name: "Ocio", icon: mdiAccountMusic },
-  { id: "6", name: "Salud", icon: mdiMedication },
-  { id: "7", name: "Suscripciones", icon: mdiCreditCardClock },
+  { id: "1", name: "Ahorro", icon: mdiFinance, color: "#ea580c" },
+  { id: "2", name: "Comida", icon: mdiSilverwareVariant, color: "#facc15" },
+  { id: "3", name: "Casa", icon: mdiHome, color: "#f472b6" },
+  { id: "4", name: "Gastos Varios", icon: mdiWalletBifold, color: "#a3e635" },
+  { id: "5", name: "Ocio", icon: mdiAccountMusic, color: "#2563eb" },
+  { id: "6", name: "Salud", icon: mdiMedication, color: "#7c3aed" },
+  {
+    id: "7",
+    name: "Suscripciones",
+    icon: mdiCreditCardClock,
+    color: "#ec4899",
+  },
 ];
--- src/types/index.ts
+++ src/types/index.ts
@@ -12,4 +12,5 @@ export type Category = {
   id: string;
   name: string;
   icon: string;
+  color: string;
 };
